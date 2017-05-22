4e4f2f7ce91404753d2a3afbee456e710c1341dd
Arjun Reddy <arjunreddy.toom@gmail.com>
2017-05-22 12:28:40 -0700

    Change1

--- server.R
+++ server.R
@@ -9,14 +9,9 @@ library(shiny)
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
+  output$Plot<- renderPlot({
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
 
   })
 
